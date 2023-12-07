2217560f4a91a63fdc942e44603faa7195eef3f1
Grégoire <gregoire.perruchotrouxel@viacesi.fr>
2023-12-07 11:32:54 +0100

    correction commande.H

--- POO-JAG/commande.h
+++ POO-JAG/commande.h
@@ -16,7 +16,6 @@ namespace NS_Comp_Mappage {
 		String^ dateemmission_commande;
 		String^ datepayement_commande;
 		String^ datelivraison_commande;
-		client^ ptrid_client;
 
 	public:
 		String^ ajouter(void);
@@ -30,8 +29,7 @@ namespace NS_Comp_Mappage {
 		void setmoyenpayement_commande(String^);
 		void setdateemission_commande(String^);
 		void setdatepayement_commande(String^);
-		void setdatelivraison_commande(String^);
-		void setptrid_client(client^);
+		void setdatelivraison_commande(String^);;
 		int^ getid_commande(void);
 		int^ getsoldereglement_commande(void);
 		client^ getptrid_client(void);
@@ -39,7 +37,6 @@ namespace NS_Comp_Mappage {
 		String^ getmoyenpayement_commande(void);
 		String^ getdatemission_commande(void);
 		String^ getdatelivraison_commande(void);
-		client^ getptrid_client(void);
 
 	};
 }
